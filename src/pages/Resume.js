68987487149d2b32313b1b21e6eba10cb762c310
bronson <disnamedoe@gmail.com>
2023-08-21 00:49:22 -0700

    added resumejs

---
+++ src/pages/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return ( 
+  <div>
+   
+    <h1 className="about">My skils!</h1>
+ 
+
+
+ <p className="Aboutme">
+   I am a front end developer who can adapt to back end, I have been learning how to code for 6 months now and in those 6 months I have learned, mysql nosql javascript html css react github
+      </p>
+
+</div>
+  );
+}
