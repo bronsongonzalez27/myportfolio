0ee538acba6cec3b5998cdb93e94a21caeeec9a1
bronson <disnamedoe@gmail.com>
2023-08-21 16:42:51 -0700

    added font size and projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,11 +9,12 @@ export default function About() {
 
 
  <p className="Aboutme">
-    I am a front end developer who can adapt to back end, I have been learning how to code for 6 months now and in those 6 months I have learned, mysql nosql javascript html css react github
+Here is a list of the projects I have worked on!
       </p>
 <h2>Projects</h2>
 <a href="https://github.com/AubreyHatake/Music-box">Music</a>
 <p>My music box project</p>
+
 </div>
   );
 }
