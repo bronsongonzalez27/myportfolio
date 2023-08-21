d0132a54cdbdd611dffa10b880acef249e383e22
bronson <disnamedoe@gmail.com>
2023-08-20 21:55:25 -0700

    fixed navbar

--- public/src/Navbar.js
+++ public/src/Navbar.js
@@ -1,15 +1,46 @@
+// Import necessary tools from a library
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
+// Create the navigation bar component for the website
 export default function Navbar() {
   return (
+    // Create a navigation section
     <nav className="nav">
-      <Link to="Bronson Gonzalez" className="site-title">
-       Bronson Gonzalez
+      {/* Create a link to the portfolio with a title */}
+      <Link to="Bronson Gonzalez" className="Portfolio-title">
+        My Portfolio
       </Link>
+      {/* Create a list of links */}
       <ul>
-        <CustomLink to="/Contact">Contact Me Here!</CustomLink>
-        <CustomLink to="/about">About me!</CustomLink>
+        {/* Add a link to the "Contact" page */}
+        <CustomLink to="/Contact">Get in Touch</CustomLink>
+        {/* Add a link to the "About" page */}
+        <CustomLink to="/about">About Me</CustomLink>
       </ul>
     </nav>
   )
 }
+
+
+// Import required tools for the link
+function CustomLink({ to, children, ...props }) {
+    // Get the path for the link
+    const resolvedPath = useResolvedPath(to);
+    
+    // Check if the link's path matches the current page's path
+    // The link should match the whole path, not just a part
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+    
+    // Create the link with a possible "active" style
+    return (
+      // If the link is active, give it a special style
+      <li className={isActive ? "active" : ""}>
+        {/* Show the link with its content */}
+        <Link to={to} {...props}>
+          {/* Show the content inside the link */}
+          {children}
+        </Link>
+      </li>
+    );
+  }
+  
