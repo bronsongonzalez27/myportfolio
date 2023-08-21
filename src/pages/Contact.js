aabd141d2dcc2363fd08c3c17781cfc48cf24292
bronson <disnamedoe@gmail.com>
2023-08-21 00:01:37 -0700

    added email and links to github

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,11 +4,20 @@ import React from "react"
 export default function Contact() {
     return <div>
       <h1>Contact me here!</h1>
+{/* list containing my github and twitter and linked in and also phone number */}
+<ul>
+<li>My phone! 3852273626</li>
+<li>My email! Jaspyyy@icloud.com</li>
+<a href="https://www.linkedin.com/in/bronson-gonzalez-327135266/"></a>
+<a href="https://github.com/bronsongonzalez27"></a>
 
+
+</ul>
+{/* form to submit email */}
 <form>
   <input type = "text" id="name" placeholder="Name "></input>
   <input type = "text" id="Email" placeholder="Email"></input>
-  
+  <button type="submit">Contact</button>
 </form>
 
    </div>
